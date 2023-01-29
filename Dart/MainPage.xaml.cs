a676cd0977a43e6e15be74bd242c748c2e6d672e
Radek P <rpietrowski1995@gmail.com>
2023-01-29 13:00:05 +0100

    Created navigation between pages, fixed grid showing

--- Dart/MainPage.xaml.cs
+++ Dart/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Dart;
+﻿using Dart.Model;
+
+namespace Dart;
 
 public partial class MainPage : ContentPage
 { 
@@ -14,11 +16,20 @@ public partial class MainPage : ContentPage
 		gameVariantPicker.ItemsSource = gameVariants;
 	}
 
-	private void startGameBt_Clicked(object sender, EventArgs e)
+	private async void startGameBt_Clicked(object sender, EventArgs e)
 	{
 		var selectedGameVariant = (int)gameVariantPicker.SelectedItem;
 		var selectedPlayersCount = (int)playersCountPicker.SelectedItem;
-		var 
+		var finishWithDouble = (string)doubleRulePicker.SelectedItem == "Włączona" ? true : false;
+		List<Player> playersList = new List<Player>();
+		for (int i = 0; i > selectedPlayersCount; i++)
+		{
+			playersList.Add(new Player(string.Format("Player {0} name", i)));
+		}
+
+		var gameModel = new GameModel(playersList, selectedGameVariant, finishWithDouble);
+		await Navigation.PushAsync(new GamePage());
+		
 	}
 }
 
